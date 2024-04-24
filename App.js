c1006143b8367217cf9acab1a4da937e1781e3ae
MarceloSilSantos <98658183+MarceloSilSantos@users.noreply.github.com>
2024-04-23 22:13:11 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hi babe</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    
+  },
+  title: {
+    color: "blue",
+    fontSize: 130
+  }
+});
